93ba20255f347b1c24b1d74ee460cc7886c41f42
David Irvine <david.irvine@maidsafe.net>
2015-03-11 12:46:30 +0000

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![crate_name = "maidsafe_types"]
+#![crate_type = "lib"]
+#[test]
+
+//! Placeholder
+
+
+/// Placeholder doc test
+pub fn always_true() -> bool { true }
+
+fn it_works() {
+ assert_eq!(always_true(), true);
+}
